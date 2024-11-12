89c2b4d2801f08210a0b7e06c817b0090428d237
Yousef Hassab <hassab.yousef@iisgalvanimi.edu.it>
2024-11-12 11:49:29 +0000

    server API con metodo post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request, jsonify
 app=Flask(__name__)
 #server web - codice per restetuire la home page
 @app.route('/')   # / = home page
@@ -6,9 +6,27 @@ def homepage():
     return render_template('homepage.html')
 
 #server API
-@app.route('/calcola', methods=['post'])
+@app.route('/calcola', methods=['POST'])
 def calcola():
-    dati=request.get_json()
+    # prendere le info dal front end 
+        json_data = request.get_json()
+        if json_data:
+            num1 = json_data.get('num1')
+            num2 = json_data.get('num2')
+            operazione = json_data.get('operazione')
+            # elaborzione delle info
+            if operazione == 'addizione' : 
+                risultato = num1 + num2
+            elif operazione == 'sottrazione' : 
+                risultato = num1 - num2
+            elif operazione == 'moltiplicazione' : 
+                risultato = num1 * num2
+            elif operazione == 'divisione' : 
+                risultato = num1 // num2py
+                # retetuire i risultati al front end
+            return jsonify(risultato = risultato)
+        else:
+            return jsonify(risultato = 'mancano i dati ')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=3245,debug=True)
